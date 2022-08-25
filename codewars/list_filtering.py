81548f8c5f7ea19ebe0473edd701659bd485a276
vipassanaecon <37123181+vipassanaecon@users.noreply.github.com>
2022-08-24 21:10:36 -0500

    Update list_filtering.py

--- codewars/list_filtering.py
+++ codewars/list_filtering.py
@@ -1,8 +1,8 @@
 #In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
-def filter_list(arg):
-    list = []
-    for x in arg:
+def filter_list(arg):   # define the function filter_listing that takes one argument (args) # function does the following:
+    list = []           # we take an empty array and assign the variable list as an empty array (arrays are mutable and not like tuples, or dictionaries)     
+    for x in arg:       # we create a for loop saying for each x in an argument lists: if x is of datatype integer the take the empty array and append it with the value of x
         if type(x) == int:
                 list.append(x)
-    return list
+    return list         # when the function is called return the list appended to the for loop. 
